729bb4ffc4fd9c00103e66e9b65546c47fa6a079
Aniket Singh <76955108+Aniket2049@users.noreply.github.com>
2024-03-16 16:53:49 +0530

    added custom component for links

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,17 +1,22 @@
 export default function Navbar() {
+  const path = window.location.pathname;
   return (
     <nav className="nav">
       <a href="/" className="site-title">
         Home
       </a>
       <ul>
-        <li>
-          <a href="/pricing">Pricing</a>
-        </li>
-        <li>
-          <a href="/about">About</a>
-        </li>
+        <CustomLink href={"/pricing"}>Pricing</CustomLink>
+        <CustomLink href={"/about"}>About</CustomLink>
       </ul>
     </nav>
+  );
+}
+
+function CustomLink({ href, children, ...props }) {
+  return (
+    <li>
+      <a href={href}>{children}</a>
+    </li>
   );
 }
