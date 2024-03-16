eb592f992efbea192663e22c5fd9d638a1c3f541
Aniket Singh <76955108+Aniket2049@users.noreply.github.com>
2024-03-16 16:48:30 +0530

    routing based on window location pathname

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
 import Navbar from "./Navbar";
+import Home from "./pages/Home";
+import Pricing from "./pages/Pricing";
+import About from "./pages/About";
 
 function App() {
-  return <Navbar />;
+  let pageComponent;
+
+  switch (window.location.pathname) {
+    case "/":
+      pageComponent = <Home />;
+      break;
+    case "/pricing":
+      pageComponent = <Pricing />;
+      break;
+    case "/about":
+      pageComponent = <About />;
+      break;
+  }
+
+  return (
+    <>
+      <Navbar />
+      {pageComponent}
+    </>
+  );
 }
 
 export default App;
